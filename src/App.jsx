bed8a8ebda04f4293b7abf47cc8b230abbe9316c
SamTombs <samuel.tombs91@outlook.com>
2025-10-12 13:54:09 +0200

    Pages, Components started

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { Routes, Route } from "react-router";
+import AppLayout from "./layout/AppLayout";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Basket from "./pages/Basket";
+import Lift from "./pages/Lift";
+import Explore from "./pages/Explore";
+import TheVault from "./pages/TheVault";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 
 
 function App() {
-
   return (
     <>
-      <h1>Hello World</h1>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/basket" element={<Basket />} />
+          <Route path="/lift" element={<Lift />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/the-vault" element={<TheVault />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,49 @@
+import { useContext } from "react";
+import { Link } from "react-router";
+
+import { UserContext } from "../contexts/UserContext";
+
+const NavBar = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleSignOut = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <nav
+      className="flex justify-between h-20 bg-[#fbfaf7e0] items-center px-4"
+    >
+      <Link to="/" className="h-6 w-50">
+        <h1>Elan</h1>
+      </Link>
+      {user ? (
+        <ul className="flex flex-row gap-6">
+          <li className="text-white bg-gradient-to-r bg-amber-900 hover:bg-gradient-to-br focus:ring-4 focus:outline-none rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="text-white bg-gradient-to-r bg-amber-900 hover:bg-gradient-to-br focus:ring-4 focus:outline-none rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            <Link to="/basket">Basket</Link>
+          </li>
+          <li className="text-white bg-gradient-to-r bg-amber-900 hover:bg-gradient-to-br focus:ring-8 focus:outline-none rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            <Link to="/" onClick={handleSignOut}>
+              Sign Out
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="flex flex-row gap-6">
+          <li className="text-white bg-gradient-to-r bg-amber-900 hover:bg-gradient-to-br focus:ring-8 focus:outline-none focus:ring-black rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            <Link to="/sign-in">Sign In</Link>
+          </li>
+          <li className="text-white bg-gradient-to-r bg-amber-900 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-black rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            <Link to="/sign-up">Sign Up</Link>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+};
+
+export default NavBar;
