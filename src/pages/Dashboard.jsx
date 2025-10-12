233cd8b073b54c003f3442f219048dab3ccb9ce8
SamTombs <samuel.tombs91@outlook.com>
2025-10-12 14:25:27 +0200

    Dashboard image inserted

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
+import ElanHomePage2 from "../assets/ElanHomePage2.jpg";
+
 export default function Dashboard() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1>Dashboard</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <img src={ElanHomePage2} alt="dashboard"  />
     </div>
   );
 }
